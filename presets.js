2e98230486fbd9ead098cf7ef03f3e36275c8628
peter <mail@pjdaniel.org.uk>
2023-04-01 21:22:33 +0000

    v3 presets

--- presets.js
+++ presets.js
@@ -1,66 +1,91 @@
-module.exports = {
-	
-	initPresets () {
-		var presets = []
-	
-		presets.push({
-			category: 'Streaming',
-			label: 'Start Stream',
-			bank: {
-				style: 'text',
-				text: 'Start Stream',
-				size: 'auto',
-				color: '16777215',
-				bgcolor: this.rgb(0, 0, 0),
+import { combineRgb } from '@companion-module/base'
+
+export function updatePresets() {
+
+	let presets = {}
+
+	presets['Start'] = {
+		type: 'button',
+		category: 'Streaming',
+		name: 'Start Stream',
+		style: {
+			text: 'Start Stream',
+			size: 'auto',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'stream',
+						options: {
+							stream_control: 'Start',
+						},
+					},
+				],
+				up: [],
 			},
-			actions: [
-				{
-					action: 'stream',
-					options: {
-						stream_control: 'Start',
-					},
+		],
+		feedbacks: [
+			{
+				feedbackId: 'streaming_state',
+				options: {
+					stream_state: 'Idle',
 				},
-			],
-			feedbacks: [
-				{
-					type: 'streaming_state',
-					options: {
-						bg: this.rgb(0, 255, 0),
-						fg: this.rgb(255, 255, 255),
-					},
+				style: {
+					bgcolor: combineRgb(0, 0, 0),
+					color: combineRgb(255, 255, 255),
 				},
-			],
-		})
-	
-		presets.push({
-			category: 'Streaming',
-			label: 'Stop Stream',
-			bank: {
-				style: 'text',
-				text: 'Stop Stream',
-				size: 'auto',
-				color: '16777215',
-				bgcolor: this.rgb(0, 0, 0),
 			},
-			actions: [
-				{
-					action: 'stream',
-					options: {
-						stream_control: 'Stop',
-					},
+			{
+				feedbackId: 'streaming_state',
+				options: {
+					stream_state: 'Connecting',
 				},
-			],
-			feedbacks: [
-				{
-					type: 'streaming_state',
-					options: {
-						bg: this.rgb(0, 255, 0),
-						fg: this.rgb(255, 255, 255),
-					},
+				style: {
+					bgcolor: combineRgb(255, 128, 0),
+					color: combineRgb(255, 255, 255),
 				},
-			],
-		})
-	
-		this.setPresetDefinitions(presets)
+			},
+			{
+				feedbackId: 'streaming_state',
+				options: {
+					stream_state: 'Streaming',
+				},
+				style: {
+					bgcolor: combineRgb(0, 204, 0),
+					color: combineRgb(255, 255, 255),
+				},
+			},
+		],
 	}
+
+	presets['Stop'] = {
+		type: 'button',
+		category: 'Streaming',
+		name: 'Stop Stream',
+		style: {
+			text: 'Stop Stream',
+			size: 'auto',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'stream',
+						options: {
+							stream_control: 'Stop',
+						},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [],
+	}
+
+	this.setPresetDefinitions(presets)
 }
